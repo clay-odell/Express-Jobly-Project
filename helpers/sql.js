e9680209d7a2c62111112bd1e1c69dd27bf7807c
Clay O'Dell <clay.odell.music@gmail.com>
2024-07-20 12:13:47 -0500

    added documentation for sql.js helper function

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,14 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Generates a SQL query for updating a subset of object properties
+ * 
+ * @param {Object} dataToUpdate - An object representing the properties to update and their new values.
+ * @param {Object} jsToSql - An object that maps JS property names to their corresponding SQL column names.
+ * @returns {Object} An object containing two properties:
+ *  - setCols: A string representing the SQL query fragment for setting the column values.
+ *  - values: An array of the new values to be set.
+ * @throws {BadRequestError} If no data is provided to update
+*/
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
