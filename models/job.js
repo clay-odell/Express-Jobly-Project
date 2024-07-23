6ed7b9e1111c98c9e326b71aa682286c84f565ab
Clay O'Dell <clay.odell.music@gmail.com>
2024-07-23 17:57:42 -0500

    added job model to job.js

--- models/job.js
+++ models/job.js
@@ -6,24 +6,91 @@ const { sqlForPartialUpdate } = require("../helpers/sql");
 
 class Job {
   static async create({ title, salary, equity, companyHandle }) {
-    const duplicateCheck = await db.query(
-      `SELECT title
-            FROM jobs
-            WHERE title = $1`,
-      [title]
-    );
-    if (duplicateCheck.rows[0]) {
-      throw new BadRequestError(`Duplicate title for: ${title}`);
-    }
     const result = await db.query(
       `INSERT INTO jobs
         (title, salary, equity, company_handle)
         VALUES($1, $2, $3, $4)
-        RETURNING title, salary, equity, company_handle AS "companyHandle"`,
+        RETURNING id, title, salary, equity, company_handle AS "companyHandle"`,
       [title, salary, equity, companyHandle]
     );
     const job = result.rows[0];
     return job;
+  }
+
+  static async findAll() {
+    const jobsRes = await db.query(
+      `SELECT id, title, salary, equity, company_handle AS "companyHandle"
+        FROM jobs
+        ORDER BY title`
+    );
+    return jobsRes.rows;
+  }
+  static async get(id) {
+    const jobRes = await db.query(
+      `SELECT id, title, salary, equity, company_handle AS "companyHandle"
+        FROM jobs
+        WHERE id = $1`,
+      [id]
+    );
+    const job = jobRes.rows[0];
+    if (!job) throw new NotFoundError(`No job: ${id}`);
+    return job;
+  }
+
+  static async findByTitle(title) {
+    const jobsRes = await db.query(
+      `SELECT id, title, salary, equity, company_handle AS "companyHandle"
+        FROM jobs
+        WHERE LOWER(title) LIKE $1`,
+      [`%${title.toLowerCase()}%`]
+    );
+    return jobsRes.rows;
+  }
+  static async minSalary(num) {
+    const jobsRes = await db.query(
+      `SELECT id, title, salary, equity, company_handle AS "companyHandle"
+        FROM jobs
+        WHERE salary >= $1
+        ORDER BY salary ASC`,
+      [num]
+    );
+    return jobsRes.rows;
+  }
+  static async update(id, data) {
+    const { setCols, values } = sqlForPartialUpdate(data, {
+      title: "title",
+      salary: "salary",
+      equity: "equity",
+    });
+    const idVarIdx = "$" + (values.length + 1);
+
+    const querySql = `UPDATE jobs
+                      SET ${setCols}
+                      WHERE id = ${idVarIdx}
+                      RETURNING id,
+                                title,
+                                salary,
+                                equity,
+                                company_handle AS "companyHandle`;
+
+    const result = await db.query(querySql, [...values, id]);
+
+    const job = result.rows[0];
+
+    if (!job) throw new NotFoundError(`No job: ${id}`);
+
+    return job;
+  }
+  static async remove(id) {
+    const result = await db.query(
+      `DELETE FROM jobs
+        WHERE id = $1
+        RETURNING id`,
+      [id]
+    );
+    const job = result.rows[0];
+
+    if (!job) throw new NotFoundError(`No job: ${id}`);
   }
 }
 
